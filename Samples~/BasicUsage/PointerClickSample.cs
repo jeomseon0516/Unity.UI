0334d0032621bf0f57f56e604405fb0171ee7092
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-07-29 14:12:40 +0900

    docs: add basic usage sample

---
+++ Samples~/BasicUsage/PointerClickSample.cs
@@ -0,0 +1,20 @@
+using Jeomseon.UI.Components;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Jeomseon.Samples.UI
+{
+    [RequireComponent(typeof(PointerClickTrigger))]
+    public sealed class PointerClickSample : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            GetComponent<PointerClickTrigger>().OnPointerClickEvent += OnClick;
+        }
+
+        private static void OnClick(PointerEventData eventData)
+        {
+            Debug.Log($"UI 클릭 위치: {eventData.position}");
+        }
+    }
+}
